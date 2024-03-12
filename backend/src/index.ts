f974cc34ebf3e61a404a1485a7a3884f70891d3c
DANKAMN <danielnicholas1477@gmail.com>
2024-03-12 06:16:06 +0100

    setup frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const PORT = 7000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Hello!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on localhost:${PORT}`);
+});
