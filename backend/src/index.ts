aa358706d853811a6b738fe43d2b79ce67fc36e2
DANKAMN <danielnicholas1477@gmail.com>
2024-03-12 06:31:41 +0100

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => console.log("Connected to database!"))
 const app = express();
 const PORT = 7000;
 
