4ddeea07d8bab6d6784d95ea3c28906eec61775e
DANKAMN <danielnicholas1477@gmail.com>
2024-03-12 10:38:32 +0100

    authenticated user links

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
-import { Menu } from 'lucide-react'
+import { CircleUserRound, Menu } from 'lucide-react'
 import { Separator } from './ui/separator'
 import { Button } from './ui/button'
 import { useAuth0 } from '@auth0/auth0-react'
+import MobileNavLinks from './MobileNavLinks'
 
 const MobileNav = () => {
-    const { loginWithRedirect } = useAuth0()
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0()
+
   return (
     <Sheet>
         <SheetTrigger>
@@ -15,16 +17,30 @@ const MobileNav = () => {
         <SheetContent className='space-y-3'>
             <SheetHeader>
                 <SheetTitle>
-                    <span>Welcome to Daniel Eats</span>
+                    {isAuthenticated ? (
+                            <span className="flex items-center font-bold gap-2">
+                                <CircleUserRound className='text-orange-500' />
+                                {user?.email}
+                            </span> 
+                        ) : (
+                            <span>Welcome to Daniel Eats</span>
+                        )
+                    }
+                    
                 </SheetTitle>
                 <Separator />
-                <SheetDescription className="flex">
-                    <Button 
-                        className="flex-1 font-bold bg-orange-500"  
-                        onClick={async () => await loginWithRedirect()}
-                    >
-                        Log In
-                    </Button>
+                <SheetDescription className="flex flex-col gap-4">
+                    {isAuthenticated ? (
+                            <MobileNavLinks />
+                        ) : (
+                            <Button 
+                                className="flex-1 font-bold bg-orange-500"  
+                                onClick={async () => await loginWithRedirect()}
+                            >
+                                Log In
+                            </Button>
+                        )
+                    }
                 </SheetDescription>
             </SheetHeader>
         </SheetContent>
---
+++ frontend/src/components/MobileNavLinks.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+import { useAuth0 } from "@auth0/auth0-react"
+
+const MobileNavLinks = () => {
+  const { logout } = useAuth0()
+  return (
+    <div className="flex flex-col gap-1">
+        <Link to="/user-profile" className="bg-white items-center font-bold hover:text-orange-500">
+            User Profile
+        </Link>
+        <Button className="flex items-center px-3 font-bold hover:bg-gray-500" onClick={() => logout()}>
+            Log Out
+        </Button>
+    </div>
+  )
+}
+
+export default MobileNavLinks
+
