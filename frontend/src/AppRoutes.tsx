f01544754b11d2ffb8e3033f5a11b261d7bc7227
DANKAMN <danielnicholas1477@gmail.com>
2024-03-12 07:44:36 +0100

    layout and header

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './layouts/layout'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<span>HOME PAGE</span>} />
+        <Route path='/' element={<Layout>
+            <span>HOME PAGE</span>
+        </Layout>} />
         <Route path='/user-profile' element={<span>USER PROFILE PAGE</span>} />
         <Route path='*' element={<Navigate to="/" />} />
     </Routes>
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className="border-b-2 border-b-orange-500 py-6">
+        <div className="container mx-auto flex justify-between items-center">
+            <Link to="/" className='text-3xl font-bold tracking-tight text=orange-500'>
+                DanielEats.com
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Header
