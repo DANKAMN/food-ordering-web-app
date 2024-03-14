bbfae28d8924f6158c7ba06691a9d945bd6863d9
DANKAMN <danielnicholas1477@gmail.com>
2024-03-14 08:16:57 +0100

    Toast notifications

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { useMutation } from "react-query"
+import { toast } from "sonner"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
@@ -68,7 +69,17 @@ export const useUpdateMyUser = () => {
         return response.json()
     }
 
-    const { mutateAsync: updateUser, isLoading, isSuccess, isError, error, reset } = useMutation(updateMyUserRequest)
+    const { mutateAsync: updateUser, isLoading, isSuccess, error, reset } = useMutation(updateMyUserRequest)
+
+    if(isSuccess) {
+        toast.success("User profile updated successfully")
+    }
+
+    if(error) {
+        toast.error(error.toString())
+        reset()
+    }
+
 
     return { updateUser, isLoading }
 }
