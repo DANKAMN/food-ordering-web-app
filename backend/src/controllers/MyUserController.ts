aef2a82d8cac02a659e5739bb0bf8dafe253e844
DANKAMN <danielnicholas1477@gmail.com>
2024-03-14 09:20:01 +0100

    Get user API

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async(req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId })
+
+    if(!currentUser) {
+      return res.status(404).json({ message: "User not found" })
+    }
+
+    res.json(currentUser)
+
+  } catch(error) {
+    console.log(error)
+    return res.status(500).json({ message: "Something went wrong" })
+  }
+}
 const createCurrentUser = async (req: Request, res: Response) => {
     try {
       // 1. check if user exists
@@ -47,5 +62,6 @@ const updateCurrentUser = async (req: Request, res: Response) => {
 }
 export default {
     createCurrentUser, 
-    updateCurrentUser
+    updateCurrentUser,
+    getCurrentUser
 }
