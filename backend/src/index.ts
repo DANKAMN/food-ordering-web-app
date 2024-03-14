404f38eb4359b9f7824d98ee2c4c14096121f4f8
DANKAMN <danielnicholas1477@gmail.com>
2024-03-14 10:25:32 +0100

    deployment tweaks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,13 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute'
+import path from 'path'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database!"))
+
+const _dirname = path.resolve()
+
 const app = express();
 const PORT = 7000;
 
@@ -17,6 +21,12 @@ app.get("/health", async (req: Request, res: Response) => {
 })
 
 app.use("/api/my/user", myUserRoute)
+
+app.use(express.static(path.join(_dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on localhost:${PORT}`);
