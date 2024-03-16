460293953138b18fd9e4e1c629bfe0b7c662e3e2
DANKAMN <danielnicholas1477@gmail.com>
2024-03-16 07:39:12 +0100

    Create restaurant API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute'
+import myRestaurantRoute from './routes/MyRestaurantRoute'
 import { v2 as cloudinary } from 'cloudinary'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -25,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 })
 
 app.use("/api/my/user", myUserRoute)
+app.use("/api/my/restaurant", myRestaurantRoute)
 
 app.listen(PORT, () => {
     console.log(`Server started on localhost:${PORT}`);
