2af7b8b031f3e4ceb356a743a5276678d5a23998
DANKAMN <danielnicholas1477@gmail.com>
2024-03-20 12:24:15 +0100

    Create restaurant API request

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,10 +1,10 @@
 export type User = {
-    _id: string;
-    email: string;
-    name: string;
-    addressLine1: string;
-    city: string,
-    country: string
+  _id: string;
+  email: string;
+  name: string;
+  addressLine1: string;
+  city: string,
+  country: string
 }
 
 export type OrderStatus =
@@ -13,3 +13,23 @@ export type OrderStatus =
   | "inProgress"
   | "outForDelivery"
   | "delivered";
+
+export type MenuItem = {
+  _id: string;
+  name: string;
+  price: number;
+}
+
+export type Restaurant = {
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+}
