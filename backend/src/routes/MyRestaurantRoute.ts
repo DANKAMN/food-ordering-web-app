2d5604b43311e645ccb4f7fcc62f884d6273f637
DANKAMN <danielnicholas1477@gmail.com>
2024-03-21 11:43:02 +0100

    Get my restaurant API

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -15,6 +15,8 @@ const upload = multer({
 })
 
 /* /api/my/restaurant */
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant)
+
 router.post(
     "/", 
     upload.single("imageFile"), 
