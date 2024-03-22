6f6fcb16bc95a732ce3b20da7221731ac5ebae70
DANKAMN <danielnicholas1477@gmail.com>
2024-03-22 09:46:30 +0100

    searchbar component

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -5,6 +5,7 @@ import AuthCallbackPage from './pages/AuthCallbackPage'
 import UserProfilePage from './pages/UserProfilePage'
 import ProtectedRoute from './auth/ProtectedRoute'
 import ManageRestaurantPage from './pages/ManageRestaurantPage'
+import SearchPage from './pages/SearchPage'
 
 const AppRoutes = () => {
   return (
@@ -14,7 +15,16 @@ const AppRoutes = () => {
             <HomePage />
           </Layout>} 
         />
+
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+
+        <Route path="/search/:city" element={
+            <Layout showHero={false}>
+              <SearchPage /> 
+            </Layout>
+          }
+        />
+
         <Route element={<ProtectedRoute />}>
           <Route path='/user-profile' element={
               <Layout>
@@ -30,6 +40,7 @@ const AppRoutes = () => {
             } 
           />
         </Route>
+
         <Route path='*' element={<Navigate to="/" />} />
     </Routes>
   )
