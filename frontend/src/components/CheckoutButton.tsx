f62a5e113080cc2f9ddee13f19f12cc0fdcaadb1
DANKAMN <danielnicholas1477@gmail.com>
2024-03-28 06:04:34 +0100

    checkout button

---
+++ frontend/src/components/CheckoutButton.tsx
@@ -0,0 +1,33 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import { useLocation } from "react-router-dom"
+import { Button } from "./ui/button"
+import LoadingButton from "./LoadingButton"
+
+
+const CheckoutButton = () => {
+  const { isAuthenticated, isLoading: isAuthLoading, loginWithRedirect } = useAuth0()
+
+  const { pathname } = useLocation()
+
+  const onLogin = async () => {
+    await loginWithRedirect({
+        appState: {
+            returnTo: pathname,
+        }
+    })
+  }
+
+  if(!isAuthenticated) {
+      return (
+        <Button className="bg-orange-500 flex-1" onClick={onLogin}>
+            Log in to checkout
+        </Button>
+      )
+  }
+
+  if(isAuthLoading) {
+    return <LoadingButton />
+  }
+}
+
+export default CheckoutButton
--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,9 +1,10 @@
 import { UseGetRestaurant } from "@/api/RestaurantApi"
+import CheckoutButton from "@/components/CheckoutButton"
 import MenuItem from "@/components/MenuItem"
 import OrderSummary from "@/components/OrderSummary"
 import RestaurantInfo from "@/components/RestaurantInfo"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
-import { Card } from "@/components/ui/card"
+import { Card, CardFooter } from "@/components/ui/card"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
 
@@ -18,7 +19,10 @@ const DetailPage = () => {
   const { restaurantId } = useParams()
   const { restaurant, isLoading } = UseGetRestaurant(restaurantId)
 
-  const [cartItems, setCartItems] = useState<CartItem[]>([])
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`)
+    return storedCartItems ? JSON.parse(storedCartItems) : []
+  })
 
   const addToCart = (menuItem: MenuItem) => {
     setCartItems((prevCartItems) => {
@@ -39,6 +43,8 @@ const DetailPage = () => {
         ]
       }
 
+      sessionStorage.setItem(`cartItems-${restaurantId}`, JSON.stringify(updatedCartItems))
+
       return updatedCartItems
     })
   }
@@ -48,6 +54,8 @@ const DetailPage = () => {
       const updatedCartItems = prevCartItems.filter(
         (item) => cartItem._id !== item._id
       )
+
+      sessionStorage.setItem(`cartItems-${restaurantId}`, JSON.stringify(updatedCartItems))
 
       return updatedCartItems
     })
@@ -76,6 +84,9 @@ const DetailPage = () => {
             <div className="">
               <Card>
                 <OrderSummary restaurant={restaurant} cartItems={cartItems} removeFromCart={removeFromCart} />
+                <CardFooter>
+                  <CheckoutButton />
+                </CardFooter>
               </Card>
             </div>
         </div>
