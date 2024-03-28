06887a843ae3a7930f8f8cf724c9c2b1c67ebfde
DANKAMN <danielnicholas1477@gmail.com>
2024-03-28 09:27:45 +0100

    Create order

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import Stripe from "stripe"
 import 'dotenv/config';
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string)
 const FRONTEND_URL = process.env.FRONTEND_URL as string
@@ -31,16 +32,26 @@ const createCheckoutSession = async (req: Request, res: Response) => {
             throw new Error("Restaurant not found")
         }
 
+        const newOrder = new Order({
+            restaurant: restaurant,
+            user: req.userId,
+            status: "placed",
+            deliveryDetails: checkoutSessionRequest.deliveryDetails,
+            cartItems: checkoutSessionRequest.cartItems,
+            createdAt: new Date()
+        })
+
         const lineItems = createLineItems(checkoutSessionRequest, restaurant.menuItems)
 
-        const session = await createSession(lineItems, "TEST_ORDER_ID", restaurant.deliveryPrice, restaurant._id.toString())
+        const session = await createSession(lineItems, newOrder._id.toString(), restaurant.deliveryPrice, restaurant._id.toString())
 
         if(!session.url) {
             return res.status(500).json({ message: "Error creating stripe session" })
         }
 
-        res.json({ url: session.url })
+        await newOrder.save()
 
+        res.json({ url: session.url })
     } catch (error: any) {
         console.log(error)
         res.status(500).json({ message: error.raw.message })
