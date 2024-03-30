4b0dad3a67f18e2819a3c1eeb95191811c958265
DANKAMN <danielnicholas1477@gmail.com>
2024-03-30 07:37:21 +0100

    Restaurant Orders page

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -101,6 +101,32 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
     }
 }
 
+const updateOrderStatus = async (req: Request, res: Response) => {
+    try {
+      const { orderId } = req.params;
+      const { status } = req.body;
+  
+      const order = await Order.findById(orderId);
+      if (!order) {
+        return res.status(404).json({ message: "order not found" });
+      }
+  
+      const restaurant = await Restaurant.findById(order.restaurant);
+  
+      if (restaurant?.user?._id.toString() !== req.userId) {
+        return res.status(401).send();
+      }
+  
+      order.status = status;
+      await order.save();
+  
+      res.status(200).json(order);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "unable to update order status" });
+    }
+}
+
 const uploadImage = async (file: Express.Multer.File) => {
     const image = file
     const base64Image = Buffer.from(image.buffer).toString("base64")
@@ -114,5 +140,6 @@ export default {
     getMyRestaurant,
     getMyRestaurantOrders,
     createMyRestaurant,
-    updateMyRestaurant
+    updateMyRestaurant,
+    updateOrderStatus
 }
--- frontend/src/api/MyRestaurantApi.tsx
+++ frontend/src/api/MyRestaurantApi.tsx
@@ -54,6 +54,58 @@ export const useGetMyRestaurantOrders = () => {
 
 }
 
+type UpdateOrderStatusRequest = {
+    orderId: string;
+    status: string;
+}
+
+export const useUpdateMyRestaurantOrder = () => {
+    const { getAccessTokenSilently } = useAuth0();
+  
+    const updateMyRestaurantOrder = async (
+      updateStatusOrderRequest: UpdateOrderStatusRequest
+    ) => {
+      const accessToken = await getAccessTokenSilently();
+  
+      const response = await fetch(
+        `${API_BASE_URL}/api/my/restaurant/order/${updateStatusOrderRequest.orderId}/status`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ status: updateStatusOrderRequest.status }),
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to update status");
+      }
+  
+      return response.json();
+    };
+  
+    const {
+      mutateAsync: updateRestaurantStatus,
+      isLoading,
+      isError,
+      isSuccess,
+      reset,
+    } = useMutation(updateMyRestaurantOrder);
+  
+    if (isSuccess) {
+      toast.success("Order updated");
+    }
+  
+    if (isError) {
+      toast.error("Unable to update order");
+      reset();
+    }
+  
+    return { updateRestaurantStatus, isLoading };
+}
+
 export const useCreateMyRestaurant = () => {
     const { getAccessTokenSilently } = useAuth0()
 
